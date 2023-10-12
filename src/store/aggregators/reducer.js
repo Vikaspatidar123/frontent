7e5f212bb3c86e4368540e4ac1ee4f53b9937b64
Hrishabh Choubey <hchoubey@grepruby.io>
2023-10-12 20:18:43 +0530

    feat: create aggregator

--- src/store/aggregators/reducer.js
+++ src/store/aggregators/reducer.js
@@ -2,12 +2,18 @@ import {
 	GET_AGGREGATORS_START,
 	GET_AGGREGATORS_FAILURE,
 	GET_AGGREGATORS_SUCCESS,
+	CREATE_AGGREGATORS_START,
+	CREATE_AGGREGATORS_FAIL,
+	CREATE_AGGREGATORS_SUCCESS,
 } from './actionTypes';
 
 const INIT_STATE = {
 	aggregatorsData: [],
 	error: {},
 	loading: true,
+	isCreateAggregatorError: false,
+	isCreateAggregatorSuccess: false,
+	isCreateAggregatorLoading: false,
 };
 
 const AggregatorsReducer = (state = INIT_STATE, { type, payload } = {}) => {
@@ -28,6 +34,28 @@ const AggregatorsReducer = (state = INIT_STATE, { type, payload } = {}) => {
 			return {
 				...state,
 				aggregatorsData: payload,
+			};
+
+		case CREATE_AGGREGATORS_START:
+			return {
+				...state,
+				isCreateAggregatorLoading: true,
+				isCreateAggregatorSuccess: false,
+			};
+
+		case CREATE_AGGREGATORS_SUCCESS:
+			return {
+				...state,
+				isCreateAggregatorLoading: false,
+				isCreateAggregatorSuccess: true,
+			};
+
+		case CREATE_AGGREGATORS_FAIL:
+			return {
+				...state,
+				isCreateAggregatorError: payload,
+				isCreateAggregatorLoading: false,
+				isCreateAggregatorSuccess: false,
 			};
 
 		default:
--- src/store/aggregators/saga.js
+++ src/store/aggregators/saga.js
@@ -1,11 +1,15 @@
 import { put, takeEvery } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
-import { GET_AGGREGATORS_START } from './actionTypes';
+import { CREATE_AGGREGATORS_START, GET_AGGREGATORS_START } from './actionTypes';
 import {
 	getAggregatorsListSuccess,
 	getAggregatorsListFailure,
+	createAggregatorSuccess,
+	createAggregatorFail,
 } from './actions';
 import { getAggregators } from '../../network/getRequests';
+import { showSnackbar } from '../snackbar/actions';
+import { createAggregator } from '../../network/postRequests';
 
 function* getAggregatorsWorker(action) {
 	try {
@@ -23,7 +27,36 @@ function* getAggregatorsWorker(action) {
 		);
 	}
 }
+
+function* createAggregatorWorker(action) {
+	try {
+		const { data } = action && action.payload;
+
+		yield createAggregator(data);
+
+		yield put(
+			showSnackbar({
+				message: `Aggregator Created Successfully`,
+				type: 'success',
+			})
+		);
+
+		yield put(createAggregatorSuccess());
+	} catch (e) {
+		yield put(createAggregatorFail());
+
+		yield put(
+			showSnackbar({
+				message: e?.response?.data?.errors[0]?.description || e.message,
+				type: 'error',
+			})
+		);
+	}
+}
+
 function* aggregatorsSaga() {
 	yield takeEvery(GET_AGGREGATORS_START, getAggregatorsWorker);
+	yield takeEvery(CREATE_AGGREGATORS_START, createAggregatorWorker);
 }
+
 export default aggregatorsSaga;
