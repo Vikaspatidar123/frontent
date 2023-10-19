c4f89dcb470cfde04c1f98fc3a2e77045843fbe6
Hrishabh Choubey <hchoubey@grepruby.io>
2023-10-19 14:46:58 +0530

    fix: tooltip

--- src/pages/LanguageManagement/LanguageManagementCol.jsx
+++ src/pages/LanguageManagement/LanguageManagementCol.jsx
@@ -1,19 +1,32 @@
 /* eslint-disable react/destructuring-assignment */
-import { Tooltip } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import '../../assets/scss/custom/pages/_language-management.scss';
+import { Tooltip } from 'reactstrap';
 
 const Keys = (cell) => (cell.value ? cell.value : '');
 
 const Action = (cell) => (cell.value ? cell.value : '');
 
-const English = (cell) =>
-	cell.value ? (
-		<Tooltip title={cell.value}>
-			<div className="english-text">{cell.value}</div>
-		</Tooltip>
+const English = (cell) => {
+	const [tooltipOpen, setTooltipOpen] = useState(false);
+	const toggle = () => setTooltipOpen(!tooltipOpen);
+
+	return cell.value ? (
+		<>
+			<div id={`text-${cell?.cell?.row?.id}`} className="english-text">
+				{cell.value}
+			</div>
+			<Tooltip
+				target={`text-${cell?.cell?.row?.id}`}
+				toggle={toggle}
+				isOpen={tooltipOpen}
+			>
+				{cell.value}
+			</Tooltip>
+		</>
 	) : (
 		''
 	);
+};
 
 export { Action, Keys, English };
