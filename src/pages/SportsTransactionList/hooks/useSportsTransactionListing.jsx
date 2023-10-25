b9f0312058487c56e582c85bf676a2b4a8ce61c5
Anil S. Chawda <achawda@grepruby.io>
2023-10-25 17:03:43 +0530

    feat: Integrated sports filters

--- src/pages/SportsTransactionList/hooks/useSportsTransactionListing.jsx
+++ src/pages/SportsTransactionList/hooks/useSportsTransactionListing.jsx
@@ -1,12 +1,22 @@
-import { useEffect, useState, useMemo } from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect, useState, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchSportsTransactionStart } from '../../../store/actions';
 import { getDateTime } from '../../../helpers/dateFormatter';
+import {
+	ActionTypes,
+	Amount,
+	CreatedAt,
+	CurrencyCode,
+	Email,
+	Id,
+	NonCashAmount,
+	Status,
+} from '../SportsTransactionListCol';
 
 const useSportsTransactionListing = () => {
 	const dispatch = useDispatch();
 	const [itemsPerPage, setItemsPerPage] = useState(10);
-	const [searchText, setSearchText] = useState('');
 	const [currentPage, setCurrentPage] = useState(1);
 	const { sportsTransaction, loading: isSportsTransactionLoading } =
 		useSelector((state) => state.SportsTransaction);
@@ -19,10 +29,9 @@ const useSportsTransactionListing = () => {
 			fetchSportsTransactionStart({
 				limit: itemsPerPage,
 				pageNo: currentPage,
-				email: searchText,
 			})
 		);
-	}, [currentPage, searchText, itemsPerPage]);
+	}, [currentPage, itemsPerPage]);
 
 	const formattedSportsTransaction = useMemo(() => {
 		const formattedValues = [];
@@ -39,9 +48,58 @@ const useSportsTransactionListing = () => {
 		return formattedValues;
 	}, [sportsTransaction]);
 
+	const columns = useMemo(
+		() => [
+			{
+				Header: 'Id',
+				accessor: 'transactionId',
+				filterable: true,
+				Cell: (cellProps) => <Id {...cellProps} />,
+			},
+			{
+				Header: 'Email',
+				accessor: 'email',
+				filterable: true,
+				Cell: (cellProps) => <Email {...cellProps} />,
+			},
+			{
+				Header: 'Amount',
+				accessor: 'amount',
+				filterable: true,
+				Cell: (cellProps) => <Amount {...cellProps} />,
+			},
+			{
+				Header: 'Non Cash Amount',
+				accessor: 'nonCashAmount',
+				filterable: true,
+				Cell: (cellProps) => <NonCashAmount {...cellProps} />,
+			},
+			{
+				Header: 'Currency Code',
+				accessor: 'currencyCode',
+				filterable: true,
+				Cell: (cellProps) => <CurrencyCode {...cellProps} />,
+			},
+			{
+				Header: 'Action Types',
+				accessor: 'actionType',
+				Cell: (cellProps) => <ActionTypes {...cellProps} />,
+			},
+			{
+				Header: 'Status',
+				accessor: 'status',
+				Cell: (cellProps) => <Status {...cellProps} />,
+			},
+			{
+				Header: 'Created At',
+				accessor: 'createdAt',
+				Cell: (cellProps) => <CreatedAt {...cellProps} />,
+			},
+		],
+		[]
+	);
+
 	return {
-		searchText,
-		setSearchText,
 		currentPage,
 		setCurrentPage,
 		totalSportsTransactionCount: sportsTransaction?.count,
@@ -49,6 +107,7 @@ const useSportsTransactionListing = () => {
 		formattedSportsTransaction,
 		itemsPerPage,
 		onChangeRowsPerPage,
+		columns,
 	};
 };
 
