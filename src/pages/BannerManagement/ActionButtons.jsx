818d7f7531a4a8346f1151f103d520b1421d52fd
Anil S. Chawda <achawda@grepruby.io>
2023-11-02 21:05:10 +0530

    fix: banner management

--- src/pages/BannerManagement/ActionButtons.jsx
+++ src/pages/BannerManagement/ActionButtons.jsx
@@ -6,15 +6,6 @@ import PropTypes from 'prop-types';
 
 const ActionButtons = ({ onClickEdit, cell }) => (
 	<ul className="list-unstyled hstack gap-1 mb-0">
-		<li data-bs-toggle="tooltip" data-bs-placement="top" title="View">
-			<Link to="/" className="btn btn-sm btn-soft-primary">
-				<i className="mdi mdi-eye-outline" id="viewtooltip" />
-			</Link>
-		</li>
-		<UncontrolledTooltip placement="top" target="viewtooltip">
-			View
-		</UncontrolledTooltip>
-
 		<li>
 			<Link
 				to="#"
@@ -24,21 +15,21 @@ const ActionButtons = ({ onClickEdit, cell }) => (
 				}}
 				className="btn btn-sm btn-soft-info"
 			>
-				<i className="mdi mdi-pencil-outline" id="edittooltip" />
-				<UncontrolledTooltip placement="top" target="edittooltip">
+				<i className="mdi mdi-pencil-outline" id={`edit-${cell?.row?.id}`} />
+				<UncontrolledTooltip placement="top" target={`edit-${cell?.row?.id}`}>
 					Edit
 				</UncontrolledTooltip>
 			</Link>
 		</li>
 
-		<li>
+		{/* <li>
 			<Link to="/" className="btn btn-sm btn-soft-danger">
 				<i className="mdi mdi-delete-outline" id="deletetooltip" />
 				<UncontrolledTooltip placement="top" target="deletetooltip">
 					Delete
 				</UncontrolledTooltip>
 			</Link>
-		</li>
+		</li> */}
 	</ul>
 );
 
