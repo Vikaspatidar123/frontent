92078109723a1e2542059d0995f4153fb45a5407
Shruti Gour <sgour@gammastack.com>
2023-11-07 06:05:19 +0000

    Fix/registration fields

--- src/pages/RegistrationFormFields/formFields.js
+++ src/pages/RegistrationFormFields/formFields.js
@@ -16,114 +16,134 @@ const getInitialValues = (defaultValue) => ({
 	sms: defaultValue?.sms || 0,
 });
 
-const leftStaticFormFields = (formFields) => [
-	{
-		name: 'email',
-		fieldType: 'toggle',
-		label: 'Email',
-		isDisabled: formFields?.disable?.includes('email'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'confirmPassword',
-		fieldType: 'toggle',
-		label: 'Confirm Password',
-		isDisabled: formFields?.disable?.includes('confirmPassword'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'firstName',
-		fieldType: 'toggle',
-		label: 'First Name',
-		isDisabled: formFields?.disable?.includes('firstName'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'dateOfBirth',
-		fieldType: 'toggle',
-		label: 'Date of Birth',
-		isDisabled: formFields?.disable?.includes('dateOfBirth'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'phone',
-		fieldType: 'toggle',
-		label: 'Phone',
-		isDisabled: formFields?.disable?.includes('phone'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'preferredLanguage',
-		fieldType: 'toggle',
-		label: 'Preferred Language',
-		isDisabled: formFields?.disable?.includes('preferredLanguage'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'countryCode',
-		fieldType: 'toggle',
-		label: 'Country',
-		isDisabled: formFields?.disable?.includes('countryCode'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'currencyCode',
-		fieldType: 'toggle',
-		label: 'Currency Code',
-		isDisabled: formFields?.disable?.includes('currencyCode'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-];
-
-const rightStaticFormFields = (formFields) => [
-	{
-		name: 'password',
-		fieldType: 'toggle',
-		label: 'Password',
-		isDisabled: formFields?.disable?.includes('password'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'username',
-		fieldType: 'toggle',
-		label: 'Username',
-		isDisabled: formFields?.disable?.includes('username'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'lastName',
-		fieldType: 'toggle',
-		label: 'Last Name',
-		isDisabled: formFields?.disable?.includes('lastName'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'address',
-		fieldType: 'toggle',
-		label: 'Address',
-		isDisabled: formFields?.disable?.includes('address'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'gender',
-		fieldType: 'toggle',
-		label: 'Gender',
-		isDisabled: formFields?.disable?.includes('gender'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'newsLetter',
-		fieldType: 'toggle',
-		label: 'News Letter',
-		isDisabled: formFields?.disable?.includes('newsLetter'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-	{
-		name: 'sms',
-		fieldType: 'toggle',
-		label: 'SMS',
-		isDisabled: formFields?.disable?.includes('sms'),
-		switchSizeClass: 'd-flex justify-content-between px-4',
-	},
-];
+const leftStaticFormFields = (formFields, callBack) => {
+	const disable = formFields?.disable;
+	return [
+		{
+			name: 'email',
+			fieldType: 'toggle',
+			label: 'Email',
+			isDisabled: disable?.includes('email'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'email'),
+		},
+		{
+			name: 'confirmPassword',
+			fieldType: 'toggle',
+			label: 'Confirm Password',
+			isDisabled: disable?.includes('confirmPassword'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'confirmPassword'),
+		},
+		{
+			name: 'firstName',
+			fieldType: 'toggle',
+			label: 'First Name',
+			isDisabled: disable?.includes('firstName'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'firstName'),
+		},
+		{
+			name: 'dateOfBirth',
+			fieldType: 'toggle',
+			label: 'Date of Birth',
+			isDisabled: disable?.includes('dateOfBirth'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'dateOfBirth'),
+		},
+		{
+			name: 'phone',
+			fieldType: 'toggle',
+			label: 'Phone',
+			isDisabled: disable?.includes('phone'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'phone'),
+		},
+		{
+			name: 'preferredLanguage',
+			fieldType: 'toggle',
+			label: 'Preferred Language',
+			isDisabled: disable?.includes('preferredLanguage'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'preferredLanguage'),
+		},
+		{
+			name: 'countryCode',
+			fieldType: 'toggle',
+			label: 'Country',
+			isDisabled: disable?.includes('countryCode'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'countryCode'),
+		},
+		{
+			name: 'currencyCode',
+			fieldType: 'toggle',
+			label: 'Currency Code',
+			isDisabled: disable?.includes('currencyCode'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'currencyCode'),
+		},
+	];
+};
+const rightStaticFormFields = (formFields, callBack) => {
+	const disable = formFields?.disable;
+	return [
+		{
+			name: 'password',
+			fieldType: 'toggle',
+			label: 'Password',
+			isDisabled: disable?.includes('password'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'password'),
+		},
+		{
+			name: 'username',
+			fieldType: 'toggle',
+			label: 'Username',
+			isDisabled: disable?.includes('username'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'username'),
+		},
+		{
+			name: 'lastName',
+			fieldType: 'toggle',
+			label: 'Last Name',
+			isDisabled: disable?.includes('lastName'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'lastName'),
+		},
+		{
+			name: 'address',
+			fieldType: 'toggle',
+			label: 'Address',
+			isDisabled: disable?.includes('address'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'address'),
+		},
+		{
+			name: 'gender',
+			fieldType: 'toggle',
+			label: 'Gender',
+			isDisabled: disable?.includes('gender'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'gender'),
+		},
+		{
+			name: 'newsLetter',
+			fieldType: 'toggle',
+			label: 'News Letter',
+			isDisabled: disable?.includes('newsLetter'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'newsLetter'),
+		},
+		{
+			name: 'sms',
+			fieldType: 'toggle',
+			label: 'SMS',
+			isDisabled: disable?.includes('sms'),
+			switchSizeClass: 'd-flex justify-content-between px-4',
+			callBack: (e) => callBack(e, 'sms'),
+		},
+	];
+};
 export { getInitialValues, leftStaticFormFields, rightStaticFormFields };
