4157e59859bce2d964f51e5d0794e25e1396bc1c
Shruti Gour <sgour@gammastack.com>
2023-11-29 10:31:09 +0000

    Feature/avatar fix

--- src/helpers/common.jsx
+++ src/helpers/common.jsx
@@ -1,4 +1,27 @@
-/* eslint-disable import/prefer-default-export */
-export const Colors = {
+const Colors = {
 	primaryBlue: '#2f06e9',
 };
+
+const colorOptions = [
+	'success',
+	'danger',
+	'warning',
+	'info',
+	'secondary',
+	'primary',
+];
+
+let lastColor = null;
+
+const getRandomColor = () => {
+	let randomColor;
+
+	do {
+		const randomIndex = Math.floor(Math.random() * colorOptions.length);
+		randomColor = colorOptions[randomIndex];
+	} while (randomColor === lastColor);
+	lastColor = randomColor;
+	return randomColor;
+};
+
+export { getRandomColor, Colors };
