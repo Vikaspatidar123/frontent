d3699acab3f21fc7c5bd2a7d98195b14086593f6
Rahul Kashyap <rkashyap@grepruby.io>
2023-12-05 08:19:04 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
 		},
 		host: true, // needed for the Docker Container port mapping to work
 		strictPort: true,
-		port: 8000, // you can replace this port with any port
+		port: 8080, // you can replace this port with any port
 	},
 	// base: './index.html',
 	build: {
