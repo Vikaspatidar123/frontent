2690e98c78de833bdac6aed7258b96b8a3c8df3b
Anil Singh Chawda <achawda@grepruby.io>
2024-02-01 20:23:28 +0530

    Fix: Banner and commented reset password.

--- src/pages/BannerManagement/formDetails.jsx
+++ src/pages/BannerManagement/formDetails.jsx
@@ -5,121 +5,121 @@ import * as Yup from 'yup';
 import { bannerType } from './constants';
 
 const getInitialValues = (defaultValue) => ({
-	bannerType: defaultValue?.bannerType || '',
-	thumbnail: defaultValue?.thumbnail || '',
+  bannerType: defaultValue?.bannerType || null,
+  thumbnail: defaultValue?.thumbnail || '',
 });
 
 const staticFormFields = [
-	{
-		name: 'thumbnail',
-		fieldType: 'file',
-		label: 'Banner',
-		showThumbnail: true,
-	},
+  {
+    name: 'thumbnail',
+    fieldType: 'file',
+    label: 'Banner',
+    showThumbnail: true,
+  },
 ];
 
 const validationSchema = ({
-	minRequiredWidth,
-	minRequiredHeight,
-	maxRequiredWidth,
-	maxRequiredHeight,
+  minRequiredWidth,
+  minRequiredHeight,
+  maxRequiredWidth,
+  maxRequiredHeight,
 }) =>
-	Yup.object().shape({
-		bannerType: Yup.string().required('Banner Type Required'),
-		thumbnail: Yup.mixed()
-			.required('Banner Required')
-			.imageDimensionCheck(
-				'Banner Required',
-				minRequiredWidth,
-				minRequiredHeight,
-				maxRequiredWidth,
-				maxRequiredHeight
-			)
-			.test('FILE_FORMAT', 'Uploaded file has unsupported format.', (value) =>
-				typeof value === 'string'
-					? true
-					: !value ||
-					  (value &&
-							[
-								'image/png',
-								'image/jpeg',
-								'image/jpg',
-								'image/svg+xml',
-							].includes(value.type))
-			),
-	});
+  Yup.object().shape({
+    bannerType: Yup.string().required('Banner Type Required'),
+    thumbnail: Yup.mixed()
+      .required('Banner Required')
+      .imageDimensionCheck(
+        'Banner Required',
+        minRequiredWidth,
+        minRequiredHeight,
+        maxRequiredWidth,
+        maxRequiredHeight
+      )
+      .test('FILE_FORMAT', 'Uploaded file has unsupported format.', (value) =>
+        typeof value === 'string'
+          ? true
+          : !value ||
+          (value &&
+            [
+              'image/png',
+              'image/jpeg',
+              'image/jpg',
+              'image/svg+xml',
+            ].includes(value.type))
+      ),
+  });
 
 const imageWidthAndHeight = (provideFile) => {
-	// take the given file (which should be an image) and return the width and height
-	const imgDimensions = { width: null, height: null };
+  // take the given file (which should be an image) and return the width and height
+  const imgDimensions = { width: null, height: null };
 
-	return new Promise((resolve) => {
-		const reader = new FileReader();
+  return new Promise((resolve) => {
+    const reader = new FileReader();
 
-		reader.readAsDataURL(provideFile);
-		reader.onload = function () {
-			const img = new Image();
-			img.src = reader.result;
+    reader.readAsDataURL(provideFile);
+    reader.onload = function () {
+      const img = new Image();
+      img.src = reader.result;
 
-			img.onload = function () {
-				imgDimensions.width = img.width;
-				imgDimensions.height = img.height;
+      img.onload = function () {
+        imgDimensions.width = img.width;
+        imgDimensions.height = img.height;
 
-				resolve(imgDimensions);
-			};
-		};
-	});
+        resolve(imgDimensions);
+      };
+    };
+  });
 };
 
 const imageDimensionCheck = Yup.addMethod(
-	Yup.mixed,
-	'imageDimensionCheck',
-	function (
-		message,
-		minRequiredWidth,
-		minRequiredHeight,
-		maxRequiredWidth,
-		maxRequiredHeight
-	) {
-		return this.test(
-			'image-width-height-check',
-			message,
-			async function (value) {
-				const { path, createError } = this;
+  Yup.mixed,
+  'imageDimensionCheck',
+  function (
+    message,
+    minRequiredWidth,
+    minRequiredHeight,
+    maxRequiredWidth,
+    maxRequiredHeight
+  ) {
+    return this.test(
+      'image-width-height-check',
+      message,
+      async function (value) {
+        const { path, createError } = this;
 
-				if (!value) {
-					return;
-				}
+        if (!value) {
+          return;
+        }
 
-				if (typeof value === 'string') {
-					return true;
-				}
+        if (typeof value === 'string') {
+          return true;
+        }
 
-				const imgDimensions = await imageWidthAndHeight(value);
+        const imgDimensions = await imageWidthAndHeight(value);
 
-				if (
-					imgDimensions.width < minRequiredWidth ||
-					imgDimensions.width > maxRequiredWidth
-				) {
-					return createError({
-						path,
-						message: `The image width needs to be between ${minRequiredWidth}px - ${maxRequiredWidth}px!`,
-					});
-				}
+        if (
+          imgDimensions.width < minRequiredWidth ||
+          imgDimensions.width > maxRequiredWidth
+        ) {
+          return createError({
+            path,
+            message: `The image width needs to be between ${minRequiredWidth}px - ${maxRequiredWidth}px!`,
+          });
+        }
 
-				if (
-					imgDimensions.height < minRequiredHeight ||
-					imgDimensions.height > maxRequiredHeight
-				) {
-					return createError({
-						path,
-						message: `The image height needs to be between ${minRequiredHeight}px - ${maxRequiredHeight}px!`,
-					});
-				}
-				return true;
-			}
-		);
-	}
+        if (
+          imgDimensions.height < minRequiredHeight ||
+          imgDimensions.height > maxRequiredHeight
+        ) {
+          return createError({
+            path,
+            message: `The image height needs to be between ${minRequiredHeight}px - ${maxRequiredHeight}px!`,
+          });
+        }
+        return true;
+      }
+    );
+  }
 );
 
 export { getInitialValues, staticFormFields, validationSchema };
