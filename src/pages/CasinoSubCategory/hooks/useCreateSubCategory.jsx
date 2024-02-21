af342050db3659d839429e1b4601697ded7e45bf
Anil Singh Chawda <achawda@grepruby.io>
2024-02-21 15:26:05 +0530

    ref: Added all dispatched action in hook for category and sub cate.

--- src/pages/CasinoSubCategory/hooks/useCreateSubCategory.jsx
+++ src/pages/CasinoSubCategory/hooks/useCreateSubCategory.jsx
@@ -1,11 +1,5 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-unused-vars */
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-
-import { Button, UncontrolledTooltip } from 'reactstrap';
 
 import { isEmpty } from 'lodash';
 import {
@@ -19,31 +13,15 @@ import {
 	editCasinoSubCategoryStart,
 	getCasinoCategoryDetailStart,
 	getLanguagesStart,
-	updateCasinoStatusStart,
-	deleteCasinoSubCategoryStart,
 } from '../../../store/actions';
-
-import {
-	GameSubCategoryId,
-	Name,
-	GameCategory,
-	ImageUrl,
-	Status,
-} from '../CasinoSubCategory';
-
 import useForm from '../../../components/Common/Hooks/useFormModal';
 import { modules } from '../../../constants/permissions';
-import usePermission from '../../../components/Common/Hooks/usePermission';
 import { formPageTitle } from '../../../components/Common/constants';
 import { decryptCredentials } from '../../../network/storageUtils';
 import { dataURLtoBlob } from '../../../utils/helpers';
 
 const useCreateSubCategory = () => {
-	const [page, setPage] = useState(1);
-	const [itemsPerPage, setItemsPerPage] = useState(10);
 	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const { isGranted, permissions } = usePermission();
 	const [langState, setLangState] = useState({ EN: '' });
 	const [showModal, setShowModal] = useState(false);
 	const [isEdit, setIsEdit] = useState({ open: false, selectedRow: '' });
@@ -98,18 +76,6 @@ const useCreateSubCategory = () => {
 			? handleEditSubCategory
 			: handleCreateSubCategory,
 	});
-
-	const handleStatus = (e, props) => {
-		e.preventDefault();
-		const { status, gameSubCategoryId } = props;
-		dispatch(
-			updateCasinoStatusStart({
-				code: 'CASINO_SUB_CATEGORY',
-				gameSubCategoryId,
-				status: !status,
-			})
-		);
-	};
 
 	const handleAddClick = (e) => {
 		e.preventDefault();
@@ -199,19 +165,6 @@ const useCreateSubCategory = () => {
 		dispatch(getCasinoCategoryDetailStart({ limit: '', pageNo: '', name: '' }));
 	}, []);
 
-	const handleAddGameClick = ({
-		e,
-		gameSubCategoryId,
-		gameCategoryName,
-		isGlobal,
-	}) => {
-		e.preventDefault();
-
-		navigate(`addGames/${gameSubCategoryId}`, {
-			state: { gameCategoryName, isGlobal },
-		});
-	};
-
 	const buttonList = useMemo(() => [
 		{
 			label: 'Create',
@@ -228,17 +181,6 @@ const useCreateSubCategory = () => {
 			operation: 'U',
 		},
 	]);
-
-	const onClickDelete = (gameSubCategoryId) => {
-		dispatch(
-			deleteCasinoSubCategoryStart({
-				gameSubCategoryId,
-				limit: itemsPerPage,
-				pageNo: page,
-				search: '',
-			})
-		);
-	};
 
 	useEffect(() => {
 		if (
@@ -274,186 +216,12 @@ const useCreateSubCategory = () => {
 					? value.size > 0
 					: !isEmpty(value) && !isEmpty(value?.EN)
 			);
-			hasFilledValues && setShowModal(!showModal);
+			if (hasFilledValues) {
+				setShowModal(!showModal);
+			}
 		}
 		setIsOpen((prev) => !prev);
 	};
-
-	const columns = useMemo(
-		() => [
-			{
-				Header: 'ID',
-				accessor: 'gameSubCategoryId',
-				filterable: true,
-				Cell: ({ cell }) => <GameSubCategoryId value={cell.value} />,
-			},
-			{
-				Header: 'NAME',
-				accessor: 'nameEN',
-				filterable: true,
-				Cell: ({ cell }) => <Name value={cell.value} />,
-			},
-			{
-				Header: 'GAME CATEGORY',
-				accessor: 'gameCategory',
-				filterable: true,
-				Cell: ({ cell }) => <GameCategory value={cell.value} />,
-			},
-			{
-				Header: 'IMAGE',
-				accessor: 'imageUrl',
-				filterable: true,
-				disableSortBy: true,
-				Cell: ({ cell }) => <ImageUrl value={cell.value} />,
-			},
-			{
-				Header: 'STATUS',
-				accessor: 'isActive',
-				filterable: true,
-				disableSortBy: true,
-				Cell: ({ cell }) => <Status value={cell.value} />,
-			},
-			{
-				Header: 'Action',
-				accessor: 'action',
-				disableFilters: true,
-				disableSortBy: true,
-				Cell: ({ cell }) => {
-					const status = cell?.row?.original?.isActive;
-					const gameSubCategoryId = cell?.row?.original?.gameSubCategoryId;
-					const gameCategoryName = cell?.row?.original?.nameEN;
-					// const isGlobal = cell?.row?.original?.isGlobal;
-					return (
-						<ul className="list-unstyled hstack gap-1 mb-0">
-							<li>
-								{status ? (
-									<Button
-										hidden={!isGranted(modules.CasinoManagement, 'T')}
-										className="btn btn-sm btn-soft-danger"
-										onClick={(e) =>
-											handleStatus(e, {
-												status,
-												gameSubCategoryId,
-											})
-										}
-									>
-										<i
-											className="mdi mdi-close-thick"
-											id={`active-${gameSubCategoryId}`}
-										/>
-										<UncontrolledTooltip
-											placement="top"
-											target={`active-${gameSubCategoryId}`}
-										>
-											Set Inactive
-										</UncontrolledTooltip>
-									</Button>
-								) : (
-									<Button
-										hidden={!isGranted(modules.CasinoManagement, 'T')}
-										className="btn btn-sm btn-soft-success"
-										onClick={(e) =>
-											handleStatus(e, {
-												status,
-												gameSubCategoryId,
-											})
-										}
-									>
-										<i
-											className="mdi mdi-check-circle"
-											id={`active-${gameSubCategoryId}`}
-										/>
-										<UncontrolledTooltip
-											placement="top"
-											target={`active-${gameSubCategoryId}`}
-										>
-											Set Active
-										</UncontrolledTooltip>
-									</Button>
-								)}
-							</li>
-
-							<li>
-								<Button
-									type="button"
-									hidden={!isGranted(modules.CasinoManagement, 'U')}
-									className="btn btn-sm btn-soft-info"
-									onClick={(e) => {
-										e.preventDefault();
-										onClickEdit(cell?.row?.original);
-									}}
-								>
-									<i
-										className="mdi mdi-pencil-outline"
-										id={`edit-${gameSubCategoryId}`}
-									/>
-									<UncontrolledTooltip
-										placement="top"
-										target={`edit-${gameSubCategoryId}`}
-									>
-										Edit
-									</UncontrolledTooltip>
-								</Button>
-							</li>
-
-							<li>
-								<Button
-									type="button"
-									hidden={!isGranted(modules.CasinoManagement, 'D')}
-									// disabled={isGlobal}
-									className="btn btn-sm btn-soft-danger"
-									onClick={(e) => {
-										e.preventDefault();
-										onClickDelete(gameSubCategoryId);
-									}}
-								>
-									<i
-										className="mdi mdi-delete-outline"
-										id={`delete-${gameSubCategoryId}`}
-									/>
-									<UncontrolledTooltip
-										placement="top"
-										target={`delete-${gameSubCategoryId}`}
-									>
-										Delete
-									</UncontrolledTooltip>
-								</Button>
-							</li>
-
-							<li>
-								<Button
-									type="button"
-									hidden={!isGranted(modules.CasinoManagement, 'T')}
-									// disabled={isGlobal}
-									className="btn btn-sm btn-soft-primary"
-									onClick={(e) =>
-										handleAddGameClick({
-											e,
-											gameSubCategoryId,
-											gameCategoryName,
-											// isGlobal,
-										})
-									}
-								>
-									<i
-										className="mdi mdi-plus-one"
-										id={`plus-one-${gameSubCategoryId}`}
-									/>
-									<UncontrolledTooltip
-										placement="top"
-										target={`plus-one-${gameSubCategoryId}`}
-									>
-										Add Games to this sub category
-									</UncontrolledTooltip>
-								</Button>
-							</li>
-						</ul>
-					);
-				},
-			},
-		],
-		[permissions]
-	);
 
 	return {
 		isOpen,
@@ -464,15 +232,8 @@ const useCreateSubCategory = () => {
 		setFormFields,
 		buttonList,
 		isCreateSubCategoryLoading,
-		handleStatus,
 		onClickEdit,
 		isEditSubCategoryLoading,
-		handleAddGameClick,
-		columns,
-		page,
-		setPage,
-		itemsPerPage,
-		setItemsPerPage,
 		toggleFormModal,
 		showModal,
 		setShowModal,
