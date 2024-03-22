53bc893ca4ce7d03d3d0acaf3f518aac87b37ad7
Shruti_gour <sgour@gammastack.com>
2024-03-22 19:13:04 +0530

    fix: transaction filters

--- src/pages/TransactionBankingList/constants.js
+++ src/pages/TransactionBankingList/constants.js
@@ -8,76 +8,12 @@ export const LEDGER_PURPOSE = [
 		value: 'Withdraw',
 	},
 	{
-		label: 'Winnings',
-		value: 'Winnings',
-	},
-	{
-		label: 'Commission',
-		value: 'Commission',
-	},
-	{
 		label: 'BonusDeposit',
 		value: 'BonusDeposit',
 	},
 	{
 		label: 'BonusWithdraw',
 		value: 'BonusWithdraw',
-	},
-	{
-		label: 'CasinoBet',
-		value: 'CasinoBet',
-	},
-	{
-		label: 'CasinoWin',
-		value: 'CasinoWin',
-	},
-	{
-		label: 'CasinoRefund',
-		value: 'CasinoRefund',
-	},
-	{
-		label: 'CasinoBonusBet',
-		value: 'CasinoBonusBet',
-	},
-	{
-		label: 'CasinoBonusWin',
-		value: 'CasinoBonusWin',
-	},
-	{
-		label: 'CasinoBonusRefund',
-		value: 'CasinoBonusRefund',
-	},
-	{
-		label: 'SportsbookBet',
-		value: 'SportsbookBet',
-	},
-	{
-		label: 'SportsbookWin',
-		value: 'SportsbookWin',
-	},
-	{
-		label: 'SportsbookRefund',
-		value: 'SportsbookRefund',
-	},
-	{
-		label: 'SportsbookCashout',
-		value: 'SportsbookCashout',
-	},
-	{
-		label: 'SportsbookExchangeBet',
-		value: 'SportsbookExchangeBet',
-	},
-	{
-		label: 'SportsbookExchangeWin',
-		value: 'SportsbookExchangeWin',
-	},
-	{
-		label: 'SportsbookExchangeRefund',
-		value: 'SportsbookExchangeRefund',
-	},
-	{
-		label: 'SportsbookExchangeCashout',
-		value: 'SportsbookExchangeCashout',
 	},
 ];
 
