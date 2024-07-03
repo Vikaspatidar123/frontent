9aa6f41769df56940193c314a1f7b5d91a411397
Anil Singh Chawda <achawda@grepruby.io>
2024-07-03 13:09:24 +0530

    feat: push notification.

--- src/pages/Notifications/components/NotifyPlayers.jsx
+++ src/pages/Notifications/components/NotifyPlayers.jsx
@@ -1,5 +1,66 @@
 import React from 'react';
+import { Col, Container, Row } from 'reactstrap';
+import Breadcrumbs from '../../../components/Common/Breadcrumb';
+import { projectName } from '../../../constants/config';
+import useNotifyPlayer from '../hooks/useNotifyPlayer';
+import FormPage from '../../../components/Common/FormPage';
+import ConfirmationModal from '../../../components/Common/ConfirmationModal';
+import { formPageTitle } from '../../../components/Common/constants';
 
-const NotifyPlayers = () => <div>NotifyPlayers</div>;
+const CreateWageringTemplate = () => {
+	// Set meta title
+	document.title = projectName;
 
-export default NotifyPlayers;
+	const {
+		validation,
+		leftFormFields,
+		rightFormFields,
+		customComponent,
+		createWageringTemplateDetailLoading,
+		showModal,
+		setShowModal,
+		navigate,
+		existingFilledData,
+	} = useNotifyPlayer();
+
+	return (
+		<div className="page-content">
+			<Container fluid>
+				<Breadcrumbs
+					title="Wagering Template"
+					breadcrumbItem="Create"
+					leftTitle={
+						<>
+							<i className="fas fa-angle-left" /> Back
+						</>
+					}
+					values={validation.values}
+					setShowModal={setShowModal}
+				/>
+				<Row>
+					<Col lg="12">
+						<FormPage
+							formTitle="Create Wagering Template"
+							validation={validation}
+							leftFormFields={leftFormFields}
+							rightFormFields={rightFormFields}
+							customComponent={customComponent}
+							submitLabel="Submit"
+							customColClasses=""
+							isSubmitLoading={createWageringTemplateDetailLoading}
+						/>
+						<ConfirmationModal
+							openModal={showModal}
+							setOpenModal={setShowModal}
+							navigate={navigate}
+							validation={existingFilledData}
+							pageType={formPageTitle.wageringTemplate}
+						/>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
+};
+
+export default CreateWageringTemplate;
---
+++ src/pages/Notifications/hooks/useNotifyPlayer.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const useNotifyPlayer = () => <div>useNotifyPlayer</div>;
+
+export default useNotifyPlayer;
