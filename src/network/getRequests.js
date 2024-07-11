fd6e1839c801db0364f6271381b8f1fed36cc8f9
Anil Singh Chawda <achawda@grepruby.io>
2024-07-11 14:20:32 +0530

    ref: updated referral enpoints.

--- src/network/getRequests.js
+++ src/network/getRequests.js
@@ -357,7 +357,7 @@ const getPaymentDetails = (payload) =>
 
 const getUserReferrals = (payload) =>
 	getRequest(
-		`${VITE_APP_API_URL}${API_NAMESPACE}${MANAGEMENT.PLAYER}referral/users`,
+		`${VITE_APP_API_URL}${API_NAMESPACE}${MANAGEMENT.BONUS}referral/users`,
 		payload
 	);
 
