6998e05a383d12bafe31e68bb0eb8f33961923e0
Anil Singh Chawda <achawda@grepruby.io>
2024-07-22 15:28:00 +0530

    feat: Added unread count.

--- src/pages/DisputeResolution/components/DisputeList.jsx
+++ src/pages/DisputeResolution/components/DisputeList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-script-url */
@@ -36,64 +37,69 @@ const DisputeList = ({
 			</h6>
 
 			<div className="mail-list mt-1">
-				{disputes?.threadTickets?.map(({ subject, id, status }) => (
-					<Link
-						to="javascript:"
-						onClick={() => setSelectedDispute(id)}
-						className={`dispute d-flex align-items-center justify-content-between ${
-							id === selectedDispute ? 'active-dispute' : ''
-						}`}
-					>
-						<div className="d-flex align-items-center w-150">
-							<span className="mdi mdi-arrow-right-drop-circle text-primary me-2" />
-							<span className="subject-ellipsis">{subject}</span>
-						</div>
-						{status ? (
-							<div>
-								<Badge
-									color={DISPUTE_STATUS_COLOR?.[status]?.color}
-									className="ms-3"
-								>
-									{`${status?.[0]?.toUpperCase()}${status.slice(1)}`}
-								</Badge>
+				{disputes?.threadTickets?.map(
+					({ subject, id, status, unread_message_count }) => (
+						<Link
+							to="javascript:"
+							onClick={() => setSelectedDispute(id)}
+							className={`dispute d-flex align-items-center justify-content-between ${
+								id === selectedDispute ? 'active-dispute' : ''
+							}`}
+						>
+							<div className="d-flex align-items-center w-150">
+								<span className="subject-ellipsis">{subject}</span>
+								&nbsp;
+								{unread_message_count ? (
+									<Badge color="danger">{unread_message_count}</Badge>
+								) : null}
 							</div>
-						) : null}
-						<div className="d-flex align-items-center">
-							<i
-								className="mdi mdi-dots-vertical font-size-18"
-								onClick={() => setDropdownOpen(id)}
-								style={{ cursor: 'pointer' }}
-							/>
-						</div>
-						{dropdownOpen === id ? (
-							<UncontrolledDropdown isOpen toggle={() => setDropdownOpen('')}>
-								<DropdownToggle
-									href="#"
-									className="card-drop"
-									tag="a"
-									style={{ display: 'none' }}
+							{status ? (
+								<div>
+									<Badge
+										color={DISPUTE_STATUS_COLOR?.[status]?.color}
+										className="ms-3"
+									>
+										{`${status?.[0]?.toUpperCase()}${status.slice(1)}`}
+									</Badge>
+								</div>
+							) : null}
+							<div className="d-flex align-items-center">
+								<i
+									className="mdi mdi-dots-vertical font-size-18"
+									onClick={() => setDropdownOpen(id)}
+									style={{ cursor: 'pointer' }}
 								/>
-								<DropdownMenu className="dropdown-menu-end">
-									{DISPUTE_STATUS?.map(({ label, value }) => (
-										<DropdownItem
-											onClick={() => {
-												updateStatus({ threadId: id, status: value });
-												setDropdownOpen('');
-											}}
-										>
-											{label}{' '}
-											{status === value ? (
-												<i className="mdi mdi-check font-size-16 text-success me-1" />
-											) : (
-												''
-											)}
-										</DropdownItem>
-									))}
-								</DropdownMenu>
-							</UncontrolledDropdown>
-						) : null}
-					</Link>
-				))}
+							</div>
+							{dropdownOpen === id ? (
+								<UncontrolledDropdown isOpen toggle={() => setDropdownOpen('')}>
+									<DropdownToggle
+										href="#"
+										className="card-drop"
+										tag="a"
+										style={{ display: 'none' }}
+									/>
+									<DropdownMenu className="dropdown-menu-end">
+										{DISPUTE_STATUS?.map(({ label, value }) => (
+											<DropdownItem
+												onClick={() => {
+													updateStatus({ threadId: id, status: value });
+													setDropdownOpen('');
+												}}
+											>
+												{label}{' '}
+												{status === value ? (
+													<i className="mdi mdi-check font-size-16 text-success me-1" />
+												) : (
+													''
+												)}
+											</DropdownItem>
+										))}
+									</DropdownMenu>
+								</UncontrolledDropdown>
+							) : null}
+						</Link>
+					)
+				)}
 				{!disputes?.threadTickets?.length && !loading ? (
 					<p className="mt-4">No disputes found!</p>
 				) : null}
