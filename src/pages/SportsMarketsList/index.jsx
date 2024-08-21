9ea62ad409fe07fc7c6b8940668002fcbf2561f1
Anil Singh Chawda <achawda@grepruby.io>
2024-08-21 18:13:04 +0530

    ref: market filters.

--- src/pages/SportsMarketsList/index.jsx
+++ src/pages/SportsMarketsList/index.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import { Card, CardBody, Col, Container, Row } from 'reactstrap';
+import { Container } from 'reactstrap';
 import TableContainer from '../../components/Common/Table';
 import Breadcrumb from '../../components/Common/Breadcrumb';
 import useSportsMarketsListing from './hooks/useSportsMarketsListing';
 import { projectName } from '../../constants/config';
-import CrudSection from '../../components/Common/CrudSection';
 import useFilters from './hooks/useFilters';
-import Filters from '../../components/Common/Filters';
 
 const SportsMarketsList = () => {
 	document.title = projectName;
 	const showBreadcrumb = useSelector((state) => state.Layout.showBreadcrumb);
 
-	const {
-		toggleAdvance,
-		isAdvanceOpen,
-		filterFields,
-		actionButtons,
-		filterValidation,
-		isFilterChanged,
-	} = useFilters();
+	const { filterValidation, filterComponent, selectedFiltersComponent } =
+		useFilters();
 
 	const {
 		currentPage,
@@ -40,37 +31,20 @@ const SportsMarketsList = () => {
 				{showBreadcrumb && (
 					<Breadcrumb title="Sportsbook" breadcrumbItem="Markets" />
 				)}
-				<Row>
-					<Col lg="12">
-						<Card>
-							<CrudSection buttonList={[]} title="Markets" />
-							<CardBody>
-								<Filters
-									validation={filterValidation}
-									filterFields={filterFields}
-									actionButtons={actionButtons}
-									isAdvanceOpen={isAdvanceOpen}
-									toggleAdvance={toggleAdvance}
-									isFilterChanged={isFilterChanged}
-								/>
-								<TableContainer
-									isLoading={isSportsMarketsLoading}
-									columns={columns}
-									data={formattedSportsMarkets}
-									isPagination
-									customPageSize={itemsPerPage}
-									paginationDiv="justify-content-center"
-									pagination="pagination justify-content-start pagination-rounded"
-									totalPageCount={totalSportsMarketsCount}
-									isManualPagination
-									onChangePagination={setCurrentPage}
-									currentPage={currentPage}
-									changeRowsPerPageCallback={onChangeRowsPerPage}
-								/>
-							</CardBody>
-						</Card>
-					</Col>
-				</Row>
+				<TableContainer
+					isLoading={isSportsMarketsLoading}
+					columns={columns}
+					data={formattedSportsMarkets}
+					isPagination
+					customPageSize={itemsPerPage}
+					totalPageCount={totalSportsMarketsCount}
+					isManualPagination
+					onChangePagination={setCurrentPage}
+					currentPage={currentPage}
+					changeRowsPerPageCallback={onChangeRowsPerPage}
+					filterComponent={filterComponent}
+					selectedFiltersComponent={selectedFiltersComponent}
+				/>
 			</Container>
 		</div>
 	);
