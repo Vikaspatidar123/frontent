508fdc8bfecb953fa889cfde0aedccba354c0467
Hritik Mandloi <hmandloi@grepruby.io>
2024-08-23 21:13:39 +0530

    feat: Left Sidebar light logo

--- src/components/VerticalLayout/Sidebar.jsx
+++ src/components/VerticalLayout/Sidebar.jsx
@@ -1,32 +1,51 @@
 // import PropTypes from 'prop-types';
 import React from 'react';
 import { withTranslation } from 'react-i18next';
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/ARC-Logo.svg';
+import LogoLight from '../../assets/images/ARC-Logo-light.svg';
+
 import withRouter from '../Common/withRouter';
 // i18n
 import SidebarContent from './SidebarContent';
 
-const Sidebar = () => (
-	<div className="vertical-menu">
-		<div className="navbar-brand-box">
-			<Link to="/" className="logo logo-light">
-				<span className="logo-sm">
-					{/* <img src={logoLightSvg} alt="" height="22" /> */}
-				</span>
-				<span className="logo-lg">
-					<img src={Logo} style={{ height: '85px', width: '115px' }} alt="" />
-				</span>
-			</Link>
-		</div>
-		<div data-simplebar className="h-100">
-			<SidebarContent />
-		</div>
+const Sidebar = () => {
+	const leftSideBarTheme = useSelector(
+		(state) => state.Layout.leftSideBarTheme
+	);
+	return (
+		<div className="vertical-menu">
+			<div className="navbar-brand-box">
+				<Link to="/" className="logo ">
+					<span className="logo-sm">
+						{/* <img src={logoLightSvg} alt="" height="22" /> */}
+					</span>
+					<span className="logo-lg">
+						{leftSideBarTheme === 'light' ? (
+							<img
+								src={LogoLight}
+								style={{ height: '85px', width: '115px' }}
+								alt=""
+							/>
+						) : (
+							<img
+								src={Logo}
+								style={{ height: '85px', width: '115px' }}
+								alt=""
+							/>
+						)}
+					</span>
+				</Link>
+			</div>
+			<div data-simplebar className="h-100">
+				<SidebarContent />
+			</div>
 
-		<div className="sidebar-background" />
-	</div>
-);
+			<div className="sidebar-background" />
+		</div>
+	);
+};
 
 Sidebar.defaultProps = {
 	// type: '',
